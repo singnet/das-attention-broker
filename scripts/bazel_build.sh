c34c4fef55062c0677618155daa8370b07ee6e2d
Andre Senna <andre.senna@gmail.com>
2024-07-30 09:52:55 -0300

    Minor fix

--- scripts/bazel_build.sh
+++ scripts/bazel_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/opt/bazel/bazelisk build --jobs 6 --noenable_bzlmod :attention-broker
-mv bazel-bin/attention-broker ../bin
+/opt/bazel/bazelisk build --jobs 6 --noenable_bzlmod :attention_broker
+mv bazel-bin/attention_broker ../bin
 /opt/bazel/bazelisk clean
 rm -f bazel-src bazel-out bazel-testlogs bazel-bin
