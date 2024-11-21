cd6c8b1c26d917ebd0f0b88deb7831103739d42c
Andre Senna <andre.senna@gmail.com>
2024-11-21 10:01:34 -0300

    WIP

--- src/query_engine/query_element/RemoteSink.cc
+++ src/query_engine/query_element/RemoteSink.cc
@@ -11,11 +11,19 @@ RemoteSink::RemoteSink(
     const string &remote_id,
     bool delete_precedent_on_destructor) : 
     Sink(precedent, "RemoteSink(" + precedent->id + ")", delete_precedent_on_destructor) {
+#ifdef DEBUG
+    cout << "RemoteSink::RemoteSink() BEGIN" << endl;
+    cout << "RemoteSink::RemoteSink() local_id: " << local_id << endl;
+    cout << "RemoteSink::RemoteSink() remote_id: " << remote_id << endl;
+#endif
 
     this->local_id = local_id;
     this->remote_id = remote_id;
     this->queue_processor = NULL;
     RemoteSink::setup_buffers();
+#ifdef DEBUG
+    cout << "RemoteSink::RemoteSink() END" << endl;
+#endif
 }
 
 RemoteSink::~RemoteSink() {
@@ -34,18 +42,27 @@ void RemoteSink::setup_buffers() {
 }
 
 void RemoteSink::graceful_shutdown() {
+#ifdef DEBUG
+    cout << "RemoteSink::graceful_shutdown() BEGIN" << endl;
+#endif
     Sink::graceful_shutdown();
     set_flow_finished();
     if (this->queue_processor != NULL) {
         this->queue_processor->join();
     }
     this->remote_output_buffer->graceful_shutdown();
+#ifdef DEBUG
+    cout << "RemoteSink::graceful_shutdown() END" << endl;
+#endif
 }
 
 // -------------------------------------------------------------------------------------------------
 // Private methods
 
 void RemoteSink::queue_processor_method() {
+#ifdef DEBUG
+    cout << "RemoteSink::queue_processor_method() BEGIN" << endl;
+#endif
     do {
         if (is_flow_finished() || 
            (this->input_buffer->is_query_answers_finished() && 
@@ -63,6 +80,12 @@ void RemoteSink::queue_processor_method() {
             Utils::sleep();
         }
     } while (true);
+#ifdef DEBUG
+    cout << "RemoteSink::queue_processor_method() ready to return" << endl;
+#endif
     this->remote_output_buffer->query_answers_finished();
     set_flow_finished();
+#ifdef DEBUG
+    cout << "RemoteSink::queue_processor_method() END" << endl;
+#endif
 }
