95e9f8af7b43b41342ffc102ab4f62170bc701da
Edgar Brissow <eddiebrissow@gmail.com>
2024-11-25 00:26:11 -0300

    Reverted files

--- scripts/bazel_test_command_line.sh
+++ scripts/bazel_test_command_line.sh
@@ -4,7 +4,7 @@ echo '==========================================================================
 if [ -z "$2" ]
     then
         echo "Running all tests in $1"
-        bazel test --jobs 16 --noenable_bzlmod tests:$1
+        /opt/bazel/bazelisk test --jobs 6 --noenable_bzlmod tests:$1
     else
         echo "Running $2 in $1"
         /opt/bazel/bazelisk test --jobs 6 --noenable_bzlmod --test_arg=--gtest_filter=$2 tests:$1
